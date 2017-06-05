9a5c583445eca1009a28ec74dd7e3e995d3df1e4
pardahlman <par.dahlman@fellowdeveloper.se>
2017-06-05 09:54:31 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.6
+RUN pip install sphinx sphinx-autobuild recommonmark sphinx_rtd_theme
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+CMD mkdir /docs
+WORKDIR /docs
+EXPOSE 8000
+ENTRYPOINT ["docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -f ./conf.py ]; then
+    echo "== Sphinx docs is not initialized =="
+    echo " - Map a volume to /docs to work on existing documentaiton"
+    echo " - Run sphinx-quickstart to create a new set of documentation"
+	tail -F /dev/null
+fi
+
+sphinx-autobuild . _build_html
